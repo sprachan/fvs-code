7683b6e6ff5ad80472218b7c447a22be61c76ed4
Sejal Prachand <sprachan@wl-151-235.wireless.umt.edu>
2025-12-10 12:00:49 -0700

    Adjust Lubrecht Analysis
    - Map study site and region
    - Only run analyses on large trees
    - Plotting adjustments for better printing

--- fia_calib/scripts/lubrecht_scripts/04_lubrecht_analysis.R
+++ fia_calib/scripts/lubrecht_scripts/04_lubrecht_analysis.R
@@ -11,6 +11,7 @@ library(dplyr)
 library(ggplot2)
 theme_set(theme_bw())
 library(patchwork)
+
 sim_out <- here('data', 'sim_outputs', 'lubrecht')
 
 # Read in data -----------------------------------------------------------------
@@ -33,9 +34,65 @@ self_mults <- readRDS(file.path(sim_out, 'self_multipliers.RData'))
 lub_treelist2023 <- readxl::read_xlsx(here('data', 'fvs_ready', 
                                        'FVS_Lubrecht_2023.xlsx'),
                                   sheet = 'FVS_TreeInit') 
+
+# 2018 actual tree list
+lub_treelist2018 <- readxl::read_xlsx(here('data', 'fvs_ready', 
+                                           'FVS_Lubrecht_2018.xlsx'),
+                                      sheet = 'FVS_TreeInit')
+
+# Spatial information
+## Fia plot locations
+fia_locs <- readRDS(here('data', 'raw_data', 'lubrecht', 'fia_plots.RData')) |>
+  terra::vect(geom = c('LONGITUDE', 'LATITUDE'),
+              crs = '+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs +type=crs') 
+
+## Study site outline
+lub_outline <- terra::vect(here('data', 'raw_data', 'lubrecht', 'shape')) |>
+  terra::project(fia_locs)
+
+## Montana outline
+mt_outline <- usmap::us_map(regions = 'county', include = 'Montana') |> # state outline 
+  terra::vect() |>
+  terra::project(fia_locs)
+
+mt_cropped <- terra::crop(mt_outline, terra::ext(-114.75, -113, 46, 47.5))
+
+## Basemaps
+basemap_site <- basemaps::basemap_terra(ext = sf::st_bbox(mt_cropped),
+                                    map_service = 'esri',
+                                    map_type = 'natgeo_world_map') |>
+  terra::project(fia_locs)
+
+basemap_mt <- basemaps::basemap_terra(ext = sf::st_bbox(mt_outline),
+                                      map_service = 'esri',
+                                      map_type = 'natgeo_world_map') |>
+  terra::project(fia_locs) |>
+  terra::mask(mt_outline)
+  
+# Visualize FIA plots ----------------------------------------------------------
+site_map <- basemaps::gg_raster(basemap_site)+
+  tidyterra::geom_spatvector(data = lub_outline, fill = 'red')+
+  tidyterra::geom_spatvector(data = fia_locs)+
+  labs(x = 'Longitude',
+       y = 'Latitude')+
+  theme_minimal()
+
+overview_map <- ggplot()+
+  tidyterra::geom_spatvector(data = mt_outline)+
+  tidyterra::geom_spatvector(data = fia_locs, size = 0.05)+
+  theme(panel.background = element_rect(fill = 'white'),
+        plot.background = element_rect(fill = NA,
+                                       color = NA),
+        panel.grid = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank())
+
+composite_map <- site_map+inset_element(overview_map, left = 0, right = 0.5,
+                       bottom = -0.1, top = 0.5)
+
 # Comparison dataframes --------------------------------------------------------
 # DBHs and heights
-comp_df <- full_join(select(uncalib_tr, -cratio, -tpa, -mcuft, -bdft),
+comp_df_full <- full_join(select(uncalib_tr, -cratio, -tpa, -mcuft, -bdft),
                      select(selfcalib_tr, -cratio, -tpa, -mcuft, -bdft),
                      by = dplyr::join_by(id, plot, age, species, TREE_ID, 
                                          STAND_ID, FIA_SPECIES, year),
@@ -52,10 +109,16 @@ comp_df <- full_join(select(uncalib_tr, -cratio, -tpa, -mcuft, -bdft),
          err_sc = dbh_sc-dbh_meas,
          err_fia = dbh_fia-dbh_meas,
          dbh_class = factor(ifelse(dbh_meas >= 3, 'LG', 'SM'))) |>
-  filter(!is.na(dbh_meas))
+  filter(!is.na(dbh_meas)) |>
+  left_join(select(lub_treelist2018, STAND_ID, TREE_ID, DIAMETER, HT),
+            by = dplyr::join_by(STAND_ID, TREE_ID)) |>
+  rename(dbh_init = DIAMETER,
+         ht_init = HT)
+  
+comp_df <- dplyr::filter(comp_df_full, dbh_init >= 3)
 
 lg <- split(comp_df, comp_df$dbh_class)$LG
-sm <- split(comp_df, comp_df$dbh_class)$SM
+sm <- split(comp_df_full, comp_df_full$dbh_class)$SM
   
 # Multipliers
 comp_mults <- dplyr::full_join(dplyr::select(fia_mults, TreeSize, SpeciesPLANTS, 
@@ -67,40 +130,73 @@ comp_mults <- dplyr::full_join(dplyr::select(fia_mults, TreeSize, SpeciesPLANTS,
 
 # Visualize comparisons --------------------------------------------------------
 comp_uc_plt <- ggplot(data = comp_df)+
-  geom_point(aes(x = dbh_meas, y = dbh_uc), alpha = 0.5)+
+  geom_point(aes(x = dbh_meas, y = dbh_uc), alpha = 0.25)+
   geom_abline(slope = 1, intercept = 0, col = 'red')+
-  labs(x = 'Measured DBH (in)', y = 'Projected DBH (in)', title = 'Uncalibrated')
+  labs(x = 'Measured DBH (in)', y = 'Projected DBH (in)')
 
 comp_sc_plt <- ggplot(data = comp_df)+
-  geom_point(aes(x = dbh_meas, y = dbh_sc), alpha = 0.5)+
+  geom_point(aes(x = dbh_meas, y = dbh_sc), alpha = 0.25)+
   geom_abline(slope = 1, intercept = 0, col = 'red')+
-  labs(x = 'Measured DBH (in)', y = 'Projected DBH (in)', title = 'Self-calibrated')
+  labs(x = 'Measured DBH (in)', y = 'Projected DBH (in)')
 
 comp_fia_plt <- ggplot(data = comp_df)+
-  geom_point(aes(x = dbh_meas, y = dbh_fia), alpha = 0.5)+
+  geom_point(aes(x = dbh_meas, y = dbh_fia), alpha = 0.25)+
   geom_abline(slope = 1, intercept = 0, col = 'red')+
-  labs(x = 'Measured DBH (in)', y = 'Projected DBH (in)', title = 'FIA calibrated')
+  labs(x = 'Measured DBH (in)', y = 'Projected DBH (in)')
 
-comp_plt <- comp_uc_plt+comp_sc_plt+comp_fia_plt+plot_layout(axes = 'collect')
+comp_plt <- comp_uc_plt/comp_sc_plt/comp_fia_plt+plot_layout(axes = 'collect')+
+  plot_annotation(tag_levels = 'a', tag_suffix = ')', tag_prefix = '(')
 
 # Visualize DBH error distributions --------------------------------------------
 uc_err_plt <- ggplot(data = comp_df)+
   geom_histogram(aes(x = err_uc), bins = 20, col = 'black', fill = 'lightgrey')+
   geom_vline(xintercept = 0, col = 'red')+
-  labs(x = 'DBH Prediction Error (inches)', title = 'Uncalibrated', y = 'Count')
+  geom_vline(xintercept = mean(comp_df$err_uc, na.rm = T), linetype = 'dashed')+
+  labs(x = 'DBH Prediction Error (in)', y = 'Count')
 
 sc_err_plt <- ggplot(data = comp_df)+
   geom_histogram(aes(x = err_sc), bins = 20, col = 'black', fill = 'lightgrey')+
   geom_vline(xintercept = 0, col = 'red')+
-  labs(x = 'DBH Prediction Error (inches)', title = 'Self-calibrated', y = 'Count')
+  geom_vline(xintercept = mean(comp_df$err_sc, na.rm = T), linetype = 'dashed')+
+  labs(x = 'DBH Prediction Error (in)', y = 'Count')
 
 fia_err_plt <- ggplot(data = comp_df)+
   geom_histogram(aes(x = err_fia), bins = 20, col = 'black', fill = 'lightgrey')+
   geom_vline(xintercept = 0, col = 'red')+
-  labs(x = 'DBH Prediction Error (inches)', title = 'FIA calibrated', y = 'Count')
+  geom_vline(xintercept = mean(comp_df$err_fia, na.rm = T), linetype = 'dashed')+
+  labs(x = 'DBH Prediction Error (in)', y = 'Count')
 
-err_hist_plt <- uc_err_plt+sc_err_plt+fia_err_plt+plot_layout(axes = 'collect')
+err_hist_plt <- uc_err_plt+sc_err_plt+fia_err_plt+
+  plot_layout(axes = 'collect')+
+  plot_annotation(tag_levels = 'a', tag_prefix = '(', tag_suffix = ')')
 
+# Visualize prediction error vs DBH --------------------------------------------
+pe_uc_plt <- ggplot(data = comp_df)+
+  geom_hline(yintercept = 0, col = 'black')+
+  geom_point(aes(x = dbh_init, y = err_uc), alpha = 0.25)+
+  geom_smooth(aes(x = dbh_init, y = err_uc), method = 'loess', se = FALSE,
+              formula = 'y~x')+
+  scale_y_continuous(limits = c(-5, 5))+
+  labs(x = 'Initial DBH (in)', y = 'Prediction Error (in)')
+
+pe_sc_plt <- ggplot(data = comp_df)+
+  geom_hline(yintercept = 0, col = 'black')+
+  geom_point(aes(x = dbh_init, y = err_sc), alpha = 0.25)+
+  geom_smooth(aes(x = dbh_init, y = err_sc), method = 'loess', se = FALSE,
+              formula = 'y~x')+
+  scale_y_continuous(limits = c(-5, 5))+
+  labs(x = 'Initial DBH (in)', y = 'Prediction Error (in)')
+
+pe_fia_plt <- ggplot(data = comp_df)+
+  geom_hline(yintercept = 0, col = 'black')+
+  geom_point(aes(x = dbh_init, y = err_fia), alpha = 0.25)+
+  geom_smooth(aes(x = dbh_init, y = err_fia), method = 'loess', se = FALSE,
+              formula = 'y~x')+
+  scale_y_continuous(limits = c(-5, 5))+
+  labs(x = 'Initial DBH (in)', y = 'Prediction Error (in)')
+
+pe_plt <- pe_uc_plt/pe_sc_plt/pe_fia_plt+plot_layout(axes = 'collect')+
+  plot_annotation(tag_levels = 'a', tag_prefix = '(', tag_suffix = ')')
 
 # Run equivalence tests --------------------------------------------------------
 # Uncalibrated
