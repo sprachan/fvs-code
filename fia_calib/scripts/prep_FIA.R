4298ab226a1d4fa6d910f6085491de0ec4dd6c7e
Sejal Prachand <sp25846@umconnect.umt.edu>
2026-03-24 17:43:48 -0600

    feat: get and run FIA data

---
+++ fia_calib/scripts/prep_FIA.R
@@ -0,0 +1,60 @@
+# DESCRIPTION ==================================================================
+#>
+#> Purpose:
+#> Extract FVS-ready data from locally-saved, state-level FIA databases.
+#> 
+#> Outputs:
+#> Save data for future use as .rds files.
+#> 
+#> Notes:
+#> Run once, or as often as new data are downloaded. 
+#>
+# ==============================================================================
+
+library(rFVSIEtools)
+library(here)
+
+# Data directories and files
+fia_path <- here('data', 'raw_data', 'fia')
+
+# Subset the FIA data ----
+fvs_ready <- list(MT = NULL, ID = NULL)
+states <- c('MT', 'ID')
+
+for(i in seq_along(states)){
+  db_path <- here(fia_path, paste0('SQLite_FIADB_', states[i], '.db'))
+  conn <- DBI::dbConnect(RSQLite::SQLite(),
+                         db_path)
+  cond_subset <- conn |>
+    dplyr::tbl('COND') |> # condition table: tells us condition
+    dplyr::filter(COND_STATUS_CD == 1, 
+                  CONDPROP_UNADJ >= 0.75, 
+                  INVYR > 2001,  
+                  DSTRBCD1 == 0,
+                  TRTCD1 == 0) |>
+    dplyr::collect()
+  DBI::dbDisconnect(conn)
+  
+  fvs_ready[[i]] <- get_FIA_state(db_path, cond_subset, add_identifier = TRUE)
+}
+
+# Process
+FVS_standInit <- dplyr::bind_rows(fvs_ready$MT$FVS_StandInit,
+                                  fvs_ready$ID$FVS_StandInit) |>
+  dplyr::arrange(PID, INV_YEAR) |>
+  dplyr::group_by(PID) |>
+  dplyr::mutate(REM_CD = dplyr::case_when(INV_YEAR == min(INV_YEAR) ~ 0,
+                            n() == 2 ~ 1,
+                            INV_YEAR == max(INV_YEAR) ~ 2,
+                            INV_YEAR != max(INV_YEAR) ~ 1,
+                            .default = NA),
+         N_REM = max(REM_CD),
+         FOREST = stringr::str_pad(FOREST, 2, pad = '0', side = 'left')) |>
+  dplyr::ungroup()
+
+FVS_treeInit <- dplyr::bind_rows(fvs_ready$MT$FVS_TreeInit,
+                                 fvs_ready$ID$FVS_TreeInit)
+
+# Save processed data
+saveRDS(FVS_standInit, file = here('data', 'fvs_ready', 'FVS_StandInit_MTID.rds'))
+saveRDS(FVS_treeInit, file = here('data', 'fvs_ready', 'FVS_TreeInit_MTID.rds'))
---
+++ fia_calib/scripts/uncalib_run.R
@@ -0,0 +1,66 @@
+# DESCRIPTION ==================================================================
+#>
+#> Purpose: Run FVS on all eligible FIA plots, with calibration turned off.
+#> This gives us a "baseline" for assessing how well calibration works and what
+#> general patterns in calibration are. Also gives us the model input for BHM.
+#> 
+#> Outputs: Two .rds objects. uc_trees_MTID.rds contains treelists for every single
+#> stand projected. uc_summary_MTID.rds contains FVS summaries for each stand.
+#> 
+#> Notes: Write function for adding REM_CD and N_REM for package?
+#>
+# ==============================================================================`
+library(here)
+library(rFVSIEtools)
+library(dplyr)
+
+fvs_ready <- here('data', 'fvs_ready')
+fvs_runs <- here('data', 'fvs_runFiles')
+fvs_dir <- file.path('C:', 'FVS', 'FVSSoftware')
+fvs_bin <- file.path(fvs_dir, 'FVSbin')
+
+# Read in stand and tree data
+#* Note: Write function for REM_CD and N_REM for package?
+
+standInit <- readRDS(here('data', 'fvs_ready', 'FVS_StandInit_MTID.rds')) |>
+  dplyr::filter(VARIANT == 'IE')
+
+treeInit <- readRDS(here('data', 'fvs_ready', 'FVS_TreeInit_MTID.rds')) |>
+  dplyr::filter() |>
+  inner_join(standInit[c('STAND_CN', 'INV_YEAR', 'REM_CD', 'N_REM')], 
+             by = 'STAND_CN')
+
+t0_stands <- standInit |>
+  dplyr::filter(N_REM >= 1, REM_CD == 0) |>
+  dplyr::select(-N_REM, -REM_CD) |>
+  as.data.frame()
+
+# only want trees associated with selected stands
+t0_trees <- treeInit |>
+  inner_join(t0_stands['STAND_CN'], by = 'STAND_CN')
+
+# Projection parameters
+num_years <- 11
+triple <- FALSE
+calibrate <- FALSE
+regen <- FALSE
+STDIDENT <- 'FIA_UNCALIB'
+random_seed <- 2025
+
+# Directory for .key, .tre, and .out files
+dt <- paste0('fia_uc_', format(Sys.Date(), '%b%d%Y'), '_', 
+             format(Sys.time(), '%H%M'), collapse = '')
+out_dir <- file.path(fvs_runs, dt)
+dir.create(out_dir)
+
+
+future::plan('multisession', workers = 5)
+
+
+sim <- run_FVS_parallel(t0_stands, t0_trees, n_batches = 4, simple_output = TRUE, 
+                     out_dir = out_dir, fvs_bin = fvs_bin, years_out = num_years,
+                     triple = triple, calibrate = calibrate, add_regen = regen,
+                     STDIDENT = STDIDENT, random_seed = random_seed)
+
+saveRDS(sim$tree_list, file = here('data', 'sim_outputs', 'uc_trees_MTID.rds'))
+saveRDS(sim$summary, file = here('data', 'sim_outputs', 'uc_summary_MTID.rds'))
