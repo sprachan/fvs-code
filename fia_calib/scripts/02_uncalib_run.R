b1b260d0c9fd90427514a8cd095805437d5ca50f
Sejal Prachand <sp25846@umconnect.umt.edu>
2026-04-08 15:35:15 -0600

    feat: rerun with new data and include history info

--- fia_calib/scripts/02_uncalib_run.R
+++ fia_calib/scripts/02_uncalib_run.R
@@ -34,6 +34,7 @@ treeInit <- readRDS(here('data', 'fvs_ready', 'FVS_TreeInit_MTID.rds')) |>
 t0_stands <- standInit |>
   dplyr::filter(N_REM >= 1) |>
   dplyr::group_by(PID) |>
+  # year of first remeasurement
   dplyr::mutate(REM_YEAR = ifelse(N_REM == 1, max(INV_YEAR), INV_YEAR[2])) |>
   dplyr::ungroup() |>
   dplyr::filter(REM_CD == 0) |>
@@ -42,7 +43,8 @@ t0_stands <- standInit |>
 # only want trees associated with selected stands
 t0_trees <- treeInit |>
   inner_join(t0_stands['STAND_CN'], by = 'STAND_CN') |>
-  filter(HISTORY == 1) # alive at first remeasurement only, don't want to project dead trees
+  filter(HISTORY == 1,
+         DIAMETER >= 3) # alive at first remeasurement only, don't want to project dead trees
 
 # Projection parameters
 num_years <- 11
@@ -78,10 +80,10 @@ tl_fvs <- sim$tree_list |>
   dplyr::mutate(REM_CD = dplyr::case_when(year == min(year) ~ 0,
                                           year == max(year) ~ 1,
                                           .default = NA)) |>
+  dplyr::arrange(year) |>
   dplyr::ungroup()
-tl_fia <- readRDS(here('data', 'fvs_ready', 'FVS_TreeInit_MTID.rds')) |>
-  dplyr::left_join(standInit[c('STAND_CN', 'INV_YEAR', 'REM_CD', 'N_REM')],
-                   by = 'STAND_CN') |>
+
+tl_fia <- treeInit |>
   dplyr::filter(TUID %in% tl_fvs$TUID)
 
 # for name matching
@@ -90,16 +92,16 @@ tl_fvs$YEAR <- tl_fvs$year
 tl_fia$YEAR <- tl_fia$INV_YEAR
 
 compare_growth <- dplyr::full_join(tl_fvs[c('DIAMETER', 'YEAR', 'TUID', 'REM_CD', 'PID')], 
-                                   tl_fia[c('DIAMETER', 'YEAR', 'TUID', 'REM_CD', 'PID')], 
-                                   by = c('TUID', 'REM_CD', 'PID'),
+                                   tl_fia[c('DIAMETER', 'YEAR', 'TUID', 'REM_CD', 'PID', 'HISTORY', 'DAMAGE1')], 
+                                   by = c('TUID', 'REM_CD', 'PID', 'YEAR'),
                                    suffix = c('_FVS', '_FIA')) |>
   dplyr::group_by(TUID, PID) |>
-  dplyr::summarize(growth_pd_FVS = YEAR_FVS[2]-YEAR_FVS[1],
-                   growth_pd_FIA = YEAR_FIA[2]-YEAR_FIA[1],
+  dplyr::summarize(growth_pd = YEAR[2]-YEAR[1],
+                   status_1 = HISTORY[1],
+                   status_2 = HISTORY[2],
                    dg_FVS = DIAMETER_FVS[2]-DIAMETER_FVS[1],
                    dg_FIA = round(DIAMETER_FIA[2]-DIAMETER_FIA[1], 2),
                    initial_dbh = DIAMETER_FIA[1]) |>
-  dplyr::filter(dg_FIA >= 0) |>
   dplyr::ungroup()
 
 saveRDS(compare_growth, file = here('data', 'sim_outputs', 'uc_compare_growth.rds'))
