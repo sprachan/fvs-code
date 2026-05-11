ca96f790b2578276a6a0a74b6f1c5bf01bc2feea
Sejal Prachand <sp25846@umconnect.umt.edu>
2026-05-11 12:31:48 -0600

    refactor, feat: include WA IE data
    - Use rFVSIEtools::fetch_cond() for more concise filtering
    - Implement looser selection criteria (>=75% forested rather than == 100%)
    - Include eastern Washington FIA plots that fall within IE variant

--- fia_calib/scripts/01a_prep_FIA.R
+++ fia_calib/scripts/01a_prep_FIA.R
@@ -19,41 +19,35 @@ library(dplyr)
 fia_path <- here('data', 'raw_data', 'fia')
 
 # Subset the FIA data ----
-fvs_ready <- list(MT = NULL, ID = NULL)
-states <- c('MT', 'ID')
+fvs_ready <- list(MT = NULL, ID = NULL, WA = NULL)
+states <- c('MT', 'ID', 'WA')
 
 for(i in seq_along(states)){
   db_path <- here(fia_path, paste0('SQLite_FIADB_', states[i], '.db'))
-  conn <- DBI::dbConnect(RSQLite::SQLite(),
-                         db_path)
-  cond_subset <- conn |>
-    tbl('COND') |> # condition table: tells us condition
-    filter(COND_STATUS_CD == 1, 
-                  CONDPROP_UNADJ == 1, 
-                  INVYR > 2001,  
-                  DSTRBCD1 == 0,
-                  TRTCD1 == 0) |>
-    collect()
-  DBI::dbDisconnect(conn)
-  
-  fvs_ready[[i]] <- get_FIA_state(db_path, cond_subset, add_identifier = TRUE)
+  # at least 3/4 subplots must be forested; only undisturbed/untreated plots
+  #> that were sampled using the newer protocol (post 2001)
+  fvs_ready[[i]] <- fetch_cond(db_path, 'COND_STATUS_CD == 1, INVYR >= 2001, CONDPROP_UNADJ >= 0.75, DSTRBCD1 == 0, TRTCD1 == 0') |>
+    get_FIA_state(db_path, fia_cond_subset = _, add_identifier = TRUE)
 }
 
 # Process
 FVS_standInit <- bind_rows(fvs_ready$MT$FVS_StandInit,
-                           fvs_ready$ID$FVS_StandInit) |>
+                           fvs_ready$ID$FVS_StandInit,
+                           fvs_ready$WA$FVS_StandInit) |>
   arrange(PID, INV_YEAR) |>
   group_by(PID) |>
   mutate(REM_CD = seq(0, n()-1),
          N_REM = max(REM_CD),
          FOREST = stringr::str_pad(FOREST, 2, pad = '0', side = 'left')) |>
+  filter(VARIANT == 'IE') |>
   ungroup()
 
 FVS_treeInit <- bind_rows(fvs_ready$MT$FVS_TreeInit,
-                                 fvs_ready$ID$FVS_TreeInit) |>
+                                 fvs_ready$ID$FVS_TreeInit,
+                          fvs_ready$WA$FVS_TreeInit) |>
   left_join(FVS_standInit[c('PID', 'INV_YEAR', 'REM_CD',  'N_REM')],
             by = c('PID', 'INV_YEAR'))
 
 # Save processed data
-saveRDS(FVS_standInit, file = here('data', 'fvs_ready', 'FVS_StandInit_MTID.rds'))
-saveRDS(FVS_treeInit, file = here('data', 'fvs_ready', 'FVS_TreeInit_MTID.rds'))
+saveRDS(FVS_standInit, file = here('data', 'fvs_ready', 'FVS_StandInit_IE.rds'))
+saveRDS(FVS_treeInit, file = here('data', 'fvs_ready', 'FVS_TreeInit_IE.rds'))
